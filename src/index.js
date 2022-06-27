d5d4b102f9dff1727f33d336f673bf17dc25b992
sumandevfunctionup <sumaneee2020@gmail.com>
2022-06-27 10:48:53 +0530

    1stpush

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require("./routes/route.js");
+const { default: mongoose } = require('mongoose');
+const app = express();
+const multer = require("multer")
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer().any())
+
+mongoose.connect("mongodb+srv://sumandev:aBosU15RXTGZYkKq@cluster0.4du2i.mongodb.net/extra?retryWrites=true&w=majority" , {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 8000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 8000))
+});
